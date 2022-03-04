3e2f91fec1205959d928f389f41dbbc7f0e7b962
nunomartinhoIPS <100919137+nunomartinhoIPS@users.noreply.github.com>
2022-03-04 09:41:21 +0000

    Nivel 4

---
+++ AppStart.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class AppStart here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class AppStart
+{
+   public static void main() {
+    System.out.println("Name\t\t: MEDIUM");
+    System.out.println("toString()\t: "+Size.MEDIUM);
+    System.out.println("Ordem\t\t: "+Size.MEDIUM.ordinal());
+    System.out.println("Valor minimo\t: "+Size.MEDIUM.getMinValue());
+    System.out.println("Valor maximo\t: "+Size.MEDIUM.getMaxValue());
+    System.out.println("Código\t\t: "+Size.MEDIUM.getCode());
+    }
+}
